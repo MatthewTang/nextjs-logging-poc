17a4995c702de17e2f08f602d8e9eb7c57aa4bf1
Tang Siu Hin <matthew.tang@connect.hku.hk>
2022-07-17 16:17:06 +0800

    added pino

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const logger = require('pino')()
+
+
 type Data = {
   name: string
 }
@@ -9,5 +12,11 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  logger.info('hello world')
+
+  const child = logger.child({ a: 'property' })
+  child.info('hello child!')
   res.status(200).json({ name: 'John Doe' })
+
 }
