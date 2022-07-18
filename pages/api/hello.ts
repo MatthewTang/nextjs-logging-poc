844552ce9bebbaefafe71eddc5b74bca9d73e612
Tang Siu Hin <matthew.tang@connect.hku.hk>
2022-07-18 23:32:53 +0800

    added pino-stackdriver/ gcp logging api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,72 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import logger from '../../logger/logger'
+//
 
-const onlypino = require('pino')()
+import { NextApiRequest, NextApiResponse } from "next"
+
+// using pino-stackdriver
+const stackdriver = require('pino-stackdriver')
+const pinoms = require('pino-multi-stream')
+// create the stackdriver destination stream
+const credentials = {
+  "private_key": process.env.GCP_PRIVATE_KEY_STRING!.replace(/\\n/g, '\n'),
+  "client_email": process.env.GCP_CLIENT_EMAIL!,
+}
+const projectId = 'test-202116'
+const writeStream = stackdriver.createWriteStream({ credentials, projectId })
+// create pino loggger
+const logger = pinoms({ streams: [{ stream: writeStream }] })
+
+
+
+// using pinco/ pinco-logflare
+// log some events logger.info('Informational message')
+//logger.error(new Error('things got bad'), 'error message')
+//import type { NextApiRequest, NextApiResponse } from 'next'
+// import logger from '../../logger/logger'
+//const onlypino = require('pino')()
 
 
 type Data = {
   name: string
 }
+
+
+
+// using gc client lib directly to call logging api
+// Imports the Google Cloud client library
+const { Logging } = require('@google-cloud/logging');
+
+async function quickstart(
+  projectId = 'test-202116', // Your Google Cloud Platform project ID
+  logName = 'my-log' // The name of the log to write to
+) {
+  // Creates a client
+  const keyFilename = './test-202116-6925c5c8412f.json';
+  const logging = new Logging({ projectId, keyFilename });
+
+  // Selects the log to write to
+  const log = logging.log(logName);
+
+  // The data to write to the log
+  const text = 'Hello, world!';
+
+  // The metadata associated with the entry
+  const metadata = {
+    resource: { type: 'global' },
+    // See: https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity
+    severity: 'INFO',
+  };
+
+  // Prepares a log entry
+  const entry = log.entry(metadata, text);
+
+  async function writeLog() {
+    // Writes the log entry
+    await log.write(entry);
+    console.log(`Logged: ${text}`);
+  }
+  writeLog();
+}
+
 
 export default function handler(
   req: NextApiRequest,
@@ -32,14 +91,16 @@ export default function handler(
   //  hi: "hello world",
   //}
 
-
   // Logging to pino-logflare
 
   logger.info(data, "Handled response. Logged with pino-logflare.")
+  //console.log(credentials);
+  //logger.info("hello");
+  //console.log(data);
+  //quickstart();
 
-
-  //const child = onlypino.child({ a: 'property' })
-  // child.info('hello child!')
+  const child = logger.child({ a: 'property' })
+  child.info('hello child!')
   res.status(200).json({ name: 'John Doe' })
 
 }
